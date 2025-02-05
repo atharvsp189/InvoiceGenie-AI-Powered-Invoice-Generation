45a954ad2f8c316faf85baa3d75c15f4fc814ca8
AtharvDevz <atharvsp189@gmail.com>
2025-02-05 10:01:50 +0530

    Minor

--- excel.py
+++ excel.py
@@ -1,4 +1,5 @@
 import json
+import openpyxl
 
 with open('invoice.json') as invoicejson:
     invoice = json.load(invoicejson)
@@ -6,20 +7,14 @@ with open('invoice.json') as invoicejson:
 with open('cell_no.json') as cell:
     cell = json.load(cell)
 
-# for val in invoice:
-#     print(val)
 
-# for val in invoice:
-#     for j in invoice[val]:
-#         print(j)
+wb = openpyxl.load_workbook("excel_file/Supplier_Form.xlsx")
+ws = wb.active # First Sheet
 
-# for val in invoice:
-#     for j in invoice[val]:
-#         if isinstance(invoice[val][j], dict):
-#             for k in invoice[val][j]:
-#                 print(k)
-#         else:
-#             print(j)
+
+print('Total number of rows: '+str(ws.max_row)+'. And total number of columns: '+str(ws.max_column))
+
+print('The value in cell B10 is: ', ws['B10'].value)
 
 
 for val in invoice:
@@ -29,7 +24,42 @@ for val in invoice:
                 cell_no = cell[val][j][k]
                 value = invoice[val][j][k]
                 print(k, " ", value, cell_no)
+                if ws[cell_no].value is None:
+                    ws[cell_no].value = value
+                else:
+                    ws[cell_no].value += value
         else:
             cell_no = cell[val][j]
             value = invoice[val][j]
             print(j, " ", value, cell_no)
+            if ws[cell_no].value is None:
+                ws[cell_no].value = value
+            else:
+                ws[cell_no].value += value
+
+
+print(ws["D17"].value)
+print(ws["C47"].value)
+print(ws["E46"].value)
+
+wb.save("Supplier.xlsx")
+wb.close()
+
+import win32com.client
+
+# Open Excel Application
+excel = win32com.client.Dispatch("Excel.Application")
+excel.Visible = False  # Run in background
+
+# Open Workbook
+wb = excel.Workbooks.Open(r"D:/Playground/Invoices-Extraction/Supplier.xlsx")
+
+# Export as PDF (change path as needed)
+pdf_path = r"D:/Playground/Invoices-Extraction/PDF/upplier.pdf"
+wb.ExportAsFixedFormat(0, pdf_path)
+
+# Close Workbook and Quit Excel
+wb.Close(False)
+excel.Quit()
+
+print(f"Excel file converted to PDF: {pdf_path}")
